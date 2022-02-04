2bfb0b7b53ee9d467a7a07af804170f4629a93d5
ezequiel <erodriguez.8226@unimar.edu.ve>
2022-02-04 11:43:07 -0400

    primer comit

---
+++ Documents/NetBeansProjects/operacion/src/operacion/Main.java
@@ -0,0 +1,14 @@
+
+package operacion;
+
+public class Main {
+    public static void main(String []args){
+        operacion op=new operacion();
+        op.leerNumeros();
+        op.sumar();
+        op.restar();
+        op.multiplicar();
+        op.dividir();
+        op.mostrar();
+    }
+}
